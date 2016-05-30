9ad0f9d10c2a8144c0c0833fcb8d43b5a1526eb6
Habby Whyte <bahybaj@hotmail.com>
2016-05-30 08:48:51 -0400

    initial commit, react set up

---
+++ app/components/header.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var header = React.createClass({
+  render: function(){
+    <div className="header">
+      <table>
+        <tr>
+          <th>LEAD Contamination</th>
+        </tr>
+      </table>
+    </div>
+  }
+})
---
+++ app/components/home.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Home = React.createClass({
+  render: function(){
+
+  }
+})
